7c8c00e38b498e62ee4e67dd2ba98171096a6495
Gabrel Andrade <leirbagdev@outlook.com>
2025-10-21 11:28:04 -0300

    repositorio

---
+++ js/form.js
@@ -0,0 +1,30 @@
+
+//class contato
+
+class contato {
+    constructor(nome, sobrenome, email, cpf, telefone, contato) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.contato = contato
+    }
+
+}
+
+function Post(form) {
+    event.preventDefault()
+    let data = new contato(form.elements.namedItem("nome").value,
+        form.elements.namedItem("sobrenome").value,
+        form.elements.namedItem("email").value,
+        form.elements.namedItem("cpf").value,
+        form.elements.namedItem("telefone").value,
+        form.elements.namedItem("contato").value);
+
+    if (data.nome != "") {
+        alert('Obrigado sr(a) ' + data.nome + ' os seus dados foram encaminhados com sucesso');
+    }
+
+    console.log(data)
+}
